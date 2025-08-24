02c92f55209cd4c67dcc7a47a247007296a0bf59
David Osipov <personal@david-osipov.vision>
2025-08-25 01:29:46 +0400

    Rename library from Resilient to Aegis Animator
    Signed-off-by: David Osipov <personal@david-osipov.vision>

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@
 // SPDX-FileCopyrightText: © 2025 David Osipov <personal@david-osipov.vision>
 
 /**
- * @file Main entry point for the Resilient Animator library.
+ * @file Main entry point for the Aegis Animator library.
  * @summary A security-first, performant, and lifecycle-aware animation utility.
  */
 
 // Core Classes
-export { ResilientAnimator } from "./animator/ResilientAnimator";
-export { AnimationManager } from "./manager/AnimationManager";
+export { AegisAnimator } from "./animator/AegisAnimator";
+export { Aegis } from "./manager/Aegis";
 
 // Core Utilities
 export { CapabilityDetector } from "./core/CapabilityDetector";
@@ -18,6 +18,6 @@ export { debounce } from "./utils/debounce";
 // Type Exports
 export * from "./types";
 
-// Default export for convenience
-import { AnimationManager } from "./manager/AnimationManager";
-export default AnimationManager;
+// Default export for convenience (e.g., import Aegis from 'aegis-animator')
+import { Aegis } from "./manager/Aegis";
+export default Aegis;
